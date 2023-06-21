2e929def955997be152faea9709bf5be4bd96064
tokiakasu <>
2023-06-21 16:00:01 +0300

    Refactor + add flag option for domain

--- cmd/subchase/subchase.go
+++ cmd/subchase/subchase.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"crypto/tls"
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"reflect"
-    "strings"
-    "net/http"
-    "net/url"
-    "crypto/tls"
-    "flag"
+	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/debug"
 	"github.com/gocolly/colly/extensions"
 )
 
@@ -42,47 +44,28 @@ func main() {
 func findDomains(givenDomain string) []string {
     var domains []string
 
-    googleQuery := "https://www.google.com/search?q=site:" + givenDomain
+    // googleQuery := "https://www.google.com/search?q=site:" + givenDomain
     yandexQuery := "https://yandex.com/search/?text=site:" + givenDomain + "&lr=100&p=0"
 
-    // Instantiate default collector
-    collector := colly.NewCollector(
+    googleCollector := colly.NewCollector(
         // colly.CacheDir("./sites_cache"),
         colly.UserAgent("Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0"),
+        colly.Async(true),
+        colly.Debugger(&debug.LogDebugger{}),
         )
-
+    
     // Referer sets valid Referer HTTP header to requests
-    extensions.Referer(collector)
-
-    // Disable TLS security check for a client
-    collector.WithTransport(&http.Transport{
-        TLSClientConfig:&tls.Config{InsecureSkipVerify: true},
-    })
-
-    // For some unknown reason, requests get "Forbidden"
-    // without using a localhost proxy (mitmproxy)
-    err := collector.SetProxy("http://localhost:8080")
-    if err != nil {
-        log.Fatalln("Error happened with proxy:", err)
-    }
-
-    // Add headers to all requests
-    collector.OnRequest(func(r *colly.Request) {
-        // log.Println("visiting", r.URL.String())
-        r.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
-        r.Headers.Add("Accept-Language", "en-US,en;q=0.5")
-        r.Headers.Add("Accept-Encoding", "gzip")
-    })
+    extensions.Referer(googleCollector)
 
     // Extract domains from Google search results
-    collector.OnHTML("#center_col cite.apx8Vc", func(e *colly.HTMLElement) {
+    googleCollector.OnHTML("#center_col cite.apx8Vc", func(e *colly.HTMLElement) {
         domSelection := e.DOM
         link := domSelection.Contents().First().Text()
         domains = append(domains, link)
     })
 
     // Find and visit next Google search results page
-    collector.OnHTML("#pnnext[href]", func(e *colly.HTMLElement) {
+    googleCollector.OnHTML("#pnnext[href]", func(e *colly.HTMLElement) {
         link := e.Attr("href")
 
         err := e.Request.Visit(link)
@@ -91,19 +74,53 @@ func findDomains(givenDomain string) []string {
         }
     })
 
+    // Set error handler
+	googleCollector.OnError(func(r *colly.Response, err error) {
+        log.Println("Error:", err)
+	})
+
+    // Instantiate Yandex collector
+    yandexCollector := googleCollector.Clone()
+    extensions.Referer(yandexCollector)
+    yandexCollector.Limit(&colly.LimitRule{
+        DomainGlob: "*yandex*",
+        Parallelism: 4,
+        Delay: 4 * time.Second,
+    })
+
+    // Disable TLS security check for a client
+    yandexCollector.WithTransport(&http.Transport{
+        TLSClientConfig:&tls.Config{InsecureSkipVerify: true},
+    })
+
+    // For some unknown reason, requests get "Forbidden"
+    // without using a localhost proxy (mitmproxy)
+    err := yandexCollector.SetProxy("localhost:80")
+    if err != nil {
+        log.Fatalln("Error happened with proxy:", err)
+    }
+
+    // Add headers to requests to Yandex
+    yandexCollector.OnRequest(func(r *colly.Request) {
+        log.Println("visiting", r.URL.String())
+        r.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+        r.Headers.Add("Accept-Language", "en-US,en;q=0.5")
+        r.Headers.Add("Accept-Encoding", "gzip")
+    })
+
     // Extract domains from Yandex search results
-    collector.OnHTML("a.Link.Link_theme_outer.Path-Item.link.path__item.link.organic__greenurl", func(e *colly.HTMLElement) {
+    yandexCollector.OnHTML("a.Link.Link_theme_outer.Path-Item.link.path__item.link.organic__greenurl", func(e *colly.HTMLElement) {
         link := e.ChildText("b")
         domains = append(domains, link)
     })
 
-    collector.OnHTML("a.link.serp-url__link.serp-url__link_bold", func(e *colly.HTMLElement) {
+    yandexCollector.OnHTML("a.link.serp-url__link.serp-url__link_bold", func(e *colly.HTMLElement) {
         link := e.Text
         domains = append(domains, link)
     })
 
     // Find and visit next Yandex search results page
-    collector.OnHTML(".Pager-Item_type_next", func(e *colly.HTMLElement) {
+    yandexCollector.OnHTML(".Pager-Item_type_next", func(e *colly.HTMLElement) {
         link := e.Attr("href")
 
         err := e.Request.Visit(link)
@@ -112,18 +129,25 @@ func findDomains(givenDomain string) []string {
         }
     })
 
+    yandexCollector.OnHTML("#checkbox-captcha-form", func(e *colly.HTMLElement) {
+        log.Println("Captcha found! Aborting parsing.")
+    })
+
     // For debug
     // collector.OnResponse(func(r *colly.Response) {
     //     log.Printf("%s\n", r.Body)
     // })
 
-    // Set error handler
-	collector.OnError(func(r *colly.Response, err error) {
+	yandexCollector.OnError(func(r *colly.Response, err error) {
         log.Println("Error:", err)
 	})
 
-    collector.Visit(googleQuery)
-    collector.Visit(yandexQuery)
+    // googleCollector.Visit(googleQuery)
+    // googleCollector.Wait()
+
+    yandexCollector.Visit(yandexQuery + "&lang=en")
+    // yandexCollector.Visit(yandexQuery + "&lang=ru")
+    yandexCollector.Wait()
 
     return domains
 }
@@ -137,8 +161,8 @@ func processFoundDomains(domains []string) []reflect.Value {
 
         if strings.Contains(element, "http") {
             u, _ := url.Parse(element)
-
             set[u.Host] = void{}
+
         } else {
             set[element] = void{}
         }
